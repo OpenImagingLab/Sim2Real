fa77c8ca72d2e7ed7504054c14f3bab74be5f559
NatureZhang <71700470+naturezhanghn@users.noreply.github.com>
2024-11-07 23:01:39 +0800

    Delete v2e_idsipf directory

--- v2e_idsipf/run_allin.sh
+++
@@ -1,5 +0,0 @@
-echo "Make training data!-----------------------------"
-python ./runv2e/runGOPROmy_v2e_train_random.py 
-
-echo "Make testing data!-----------------------------"
-python ./runv2e/runGOPROmy_v2e_test_random.py
